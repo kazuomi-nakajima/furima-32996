14258944237a41e101fd9ebe09c42afa68d523da
kazuomi-nakajima <x15076.nakajima@gmail.com>
2021-01-08 15:47:29 +0900

    Js：価格計算機能実装

---
+++ app/javascript/item/price.js
@@ -0,0 +1,29 @@
+function price_calculation() {   // 料金計算関数
+
+  console.log ("読み込み確認");   //読み込まれたか確認用ログ
+  const price = document.getElementById("item-price");
+
+  price.addEventListener("keyup", () =>{
+    const money = parseInt(price.value, 10);
+    
+    //手数料・利益フォーム参照
+    const tax = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
+
+    //手数料・利益計算
+    let taxPrice =  Math.floor(money * 0.1);
+    let profitPrice = Math.floor(money - taxPrice);
+
+    //HTMLに表示
+    if(!isNaN(taxPrice)){
+      tax.innerHTML = `${taxPrice}`;
+      profit.innerHTML = `${profitPrice}`;
+    }else {       //入力が数値以外が入力されたら計算しない
+      tax.innerHTML = ``;
+      profit.innerHTML = ``;
+    }
+  })
+}
+
+//ページが読み込まれた時price_calculationの処理を実行
+window.addEventListener('load', price_calculation);
