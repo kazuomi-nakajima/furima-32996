ed3c825902e6bcb12636c148eebbbcb9a62c16b8
kazuomi-nakajima <x15076.nakajima@gmail.com>
2021-01-19 11:57:44 +0900

    テストコード修正

--- spec/models/order_processing_spec.rb
+++ spec/models/order_processing_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe OrderProcessing, type: :model do
   before do
-    @order_processing = FactoryBot.build(:order_processing)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order_processing = FactoryBot.build(:order_processing, user_id: @user.id, item_id: @item.id)
+    sleep(0.001)
   end
 
   describe '購入手続き' do
     context '購入できる時' do
-      it 'token, postal_code, prefecture_id, municipal, address, building_name, phone_numberが存在している' do
+      it 'token, postal_code, prefecture_id, municipal, address, building_name, phone_number,user,itemが存在している' do
         expect(@order_processing).to be_valid
       end
       it 'building_nameが空でも登録できる' do
@@ -25,6 +28,7 @@ RSpec.describe OrderProcessing, type: :model do
       it 'トークンが空' do
         @order_processing.token = ''
         @order_processing.valid?
+        # binding.pry
         expect(@order_processing.errors.full_messages).to include("Token can't be blank")
       end
       # /token
@@ -36,12 +40,12 @@ RSpec.describe OrderProcessing, type: :model do
         expect(@order_processing.errors.full_messages).to include("Postal code can't be blank",
                                                                   'Postal code is invalid. Include hyphen(-)')
       end
-      it '-がない' do
+      it '郵便番号に-がない' do
         @order_processing.postal_code = '0000000'
         @order_processing.valid?
         expect(@order_processing.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
       end
-      it '3桁 - 4桁 ではない' do
+      it '郵便番号が[3桁 - 4桁] ではない' do
         @order_processing.postal_code = '00-00000'
         @order_processing.valid?
         expect(@order_processing.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
@@ -109,6 +113,32 @@ RSpec.describe OrderProcessing, type: :model do
         expect(@order_processing.errors.full_messages).to include('Phone number is invalid')
       end
       # /phone_number
+
+      # user
+      it 'ユーザーがnil' do
+        @order_processing.user_id = nil
+        @order_processing.valid?
+        expect(@order_processing.errors.full_messages).to include("User can't be blank")
+      end
+      it 'ユーザーが空' do
+        @order_processing.user_id = ''
+        @order_processing.valid?
+        expect(@order_processing.errors.full_messages).to include("User can't be blank")
+      end
+      # /user
+
+      # item
+      it '商品がnil' do
+        @order_processing.item_id = nil
+        @order_processing.valid?
+        expect(@order_processing.errors.full_messages).to include("Item can't be blank")
+      end
+      it '商品が空' do
+        @order_processing.item_id = ''
+        @order_processing.valid?
+        expect(@order_processing.errors.full_messages).to include("Item can't be blank")
+      end
+      # /item
     end
   end
 end
